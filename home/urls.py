640383c5742a29f62cd4498db9fe5a568903c6e1
18jayanth <jayanthtulugu@gmail.com>
2025-11-11 22:39:56 +0530

    created structure of project with bootstraped html pages

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from home.views import *
 urlpatterns = [
     path("",index,name='index'),
+    path("login/",login,name='login'),
+    path("register/",register,name='register'),
 ]
